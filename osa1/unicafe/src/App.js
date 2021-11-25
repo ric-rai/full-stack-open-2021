8d39bbe9b8ec41d92a506403e2012ee5756aedff
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2021-11-25 22:38:43 +0200

    exercise 1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -4,7 +4,7 @@ const Header = ({ header }) => <h1>{header}</h1>
 
 const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>
 
-const StatisticLine = ({ text, value }) => <p>{text + " " + value}</p>
+const StatisticLine = ({ text, value }) => <tr><td>{text}</td><td>{value}</td></tr>
 
 const Statistics = (props) => {
     const { good, neutral, bad } = props
@@ -13,14 +13,16 @@ const Statistics = (props) => {
     const posPercent = () => (good / sum() * 100).toFixed(2)
 
     return sum() === 0 ? <p>No feedback given</p> : (
-        <div>
-            <StatisticLine text={"good"} value={good}/>
-            <StatisticLine text={"neutral"} value={neutral}/>
-            <StatisticLine text={"bad"} value={bad}/>
-            <StatisticLine text={"all"} value={sum()}/>
-            <StatisticLine text={"average"} value={average()}/>
-            <StatisticLine text={"positive"} value={posPercent() + " %"}/>
-        </div>
+        <table>
+            <tbody>
+                <StatisticLine text={"good"} value={good}/>
+                <StatisticLine text={"neutral"} value={neutral}/>
+                <StatisticLine text={"bad"} value={bad}/>
+                <StatisticLine text={"all"} value={sum()}/>
+                <StatisticLine text={"average"} value={average()}/>
+                <StatisticLine text={"positive"} value={posPercent() + " %"}/>
+            </tbody>
+        </table>
     )
 }
 
