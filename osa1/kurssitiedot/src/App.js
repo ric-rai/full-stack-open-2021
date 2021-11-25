c85d1b1337b100aca9ba25801c815d5966518539
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2021-11-25 17:31:22 +0200

    exercise 1.3

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -4,9 +4,9 @@ const Header = (props) => <h1>{props.course}</h1>
 
 const Content = (props) => (
     <div>
-        <Part part={props.data[0].part} exercise={props.data[0].exercise} />
-        <Part part={props.data[1].part} exercise={props.data[1].exercise} />
-        <Part part={props.data[2].part} exercise={props.data[2].exercise} />
+        <Part part={props.data[0].name} exercise={props.data[0].exercises} />
+        <Part part={props.data[1].name} exercise={props.data[1].exercises} />
+        <Part part={props.data[2].name} exercise={props.data[2].exercises} />
     </div>
 )
 
@@ -16,19 +16,24 @@ const Total = (props) => <p>Number of exercises {props.numberOfExercises}</p>
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     return (
         <div>
            <Header course={course} />
-            <Content data={[1, 2, 3].map(n =>
-                ({part: eval("part" + n), exercise: eval("exercises" + n)}))} />
-            <Total numberOfExercises={exercises1 + exercises2 + exercises3} />
+            <Content data={[part1, part2, part3]} />
+            <Total numberOfExercises={part1.exercises + part2.exercises + part3.exercises} />
         </div>
     )
 }
