b1b57aa335636efa0dacf854c8d6cc42cb572574
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2021-11-27 15:31:36 +0200

    exercise 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react'
 
-const Person = ({name}) => <h4>{name}</h4>
+const Person = ({name, numer}) => <p>{name} {numer}</p>
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        {name: 'Arto Hellas', number: "040-1231244"}
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
-    const handleNewName = e => setNewName(e.target.value)
+    const handleInputChange = (setter) => e => setter(e.target.value)
 
     const addPerson = e => {
         e.preventDefault()
         if (persons.map(p => p.name).includes(newName))
             alert(`${newName} is already added to phonebook`)
         else {
-            setPersons(persons.concat({name: newName}))
+            setPersons(persons.concat({name: newName, number: newNumber}))
             setNewName('')
+            setNewNumber('')
         }
     }
 
@@ -25,17 +27,17 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={addPerson}>
                 <div>name:
-                    <input
-                        value={newName}
-                        onChange={handleNewName}
-                    />
+                    <input value={newName} onChange={handleInputChange(setNewName)}/>
+                </div>
+                <div> number:
+                    <input value={newNumber} onChange={handleInputChange(setNewNumber)}/>
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(p => <Person key={p.name} name={p.name}/>)}
+            {persons.map(p => <Person key={p.name} name={p.name} numer={p.number}/>)}
         </div>
     )
 
