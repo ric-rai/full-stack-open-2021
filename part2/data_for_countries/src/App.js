d9c0341cee2688bdf6b22e5ff3081b770200841d
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-03-26 14:31:23 +0200

    exercise 2.13

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios"
+const api_key = process.env.REACT_APP_API_KEY
 
 const Input = ({label, value, onChange}) => (
     <div>{label}:{" "}
@@ -32,16 +33,36 @@ const CountryData = ({countryData}) => {
                 <ul>{Object.values(countryData.languages).map(l => <li key={l}>{l}</li>)}</ul>
                 <img src={countryData.flags.png} width="200"/> 
             </div>
+            <Weather capital={countryData.capital[0]}/>
         </div>
     )
 } 
+
+const Weather = ({capital}) => {
+    const [weather, setWeather] = useState(null)
+
+    const fetchWeather = () => void axios
+        .get(`http://api.openweathermap.org/data/2.5/weather?units=metric&q=${capital}&appid=${api_key}`)
+        .then(r => (setWeather(r.data)))
+    useEffect(fetchWeather, []) 
+
+    if (!weather) return null
+    else return (
+        <div>
+            <h2>Weather in {weather.name}</h2>
+            <div>temperature {weather.main.temp} Celsius</div>
+            <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+            <div>wind {weather.wind.speed} m/s</div>
+        </div>
+    )
+}
 
 const App = () => {
     const [countries, setCountries] = useState([])
     const [countryNames, setCountryNames] = useState([])
     const [filtered, setFiltered] = useState([])
     const [currentFilter, setCurrentFilter] = useState('')
-    const [validationMsg, setValidationMsg] = useState('')
+    const [msg, setMsg] = useState('')
     const [country, setCountry] = useState(null)
 
     const fetchCountries = () => void axios
@@ -52,7 +73,7 @@ const App = () => {
     const handleFilterChange = e => changeFilter(e.target.value)
     const getCountry = name => countries.find(c => c.name.common === name) 
     const setFilterStates = (f = [], msg = "", coName = null) => 
-        (setFiltered(f), setCountry(getCountry(coName)), setValidationMsg(msg))
+        (setFiltered(f), setCountry(getCountry(coName)), setMsg(msg))
     const handleCountryClick = name => () => setFilterStates(filtered, "", name)
     const filter = (value)  => {
         const fNames = countryNames.filter(n => n.match(new RegExp(value, 'i')))
@@ -70,7 +91,7 @@ const App = () => {
     return (
         <div>
             <Input label={'find countries'} value={currentFilter} onChange={handleFilterChange}/>
-            <Message msg={validationMsg}/>
+            <Message msg={msg}/>
             <CountryNames names={filtered} handleCountryClick={handleCountryClick}/>
             <CountryData countryData={country}/>
         </div>
