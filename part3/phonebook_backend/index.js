a7c8f2ddff6fb63293ae0dc48be544671ff98332
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-03-29 17:12:37 +0300

    exercise 3.2

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/info', (req, res) => res.send(`
+    <div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    </div>
+`))
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
